0cb22a3c0c1cbda3d76fbebc607652d163294263
Pasham Varshitha <varshithapasham552@gmail.com>
2026-02-11 21:18:49 +0530

    Add files via upload

---
+++ factorial.java
@@ -0,0 +1,19 @@
+package helloworld;
+
+import java.util.Scanner;
+
+public class factorial {
+	public static void main(String[] args) {
+		
+	Scanner sc = new Scanner(System.in);
+	System.out.println("enter a number");
+	int num = sc.nextInt();
+	int factorial = 1;
+	for ( int i=1;i<=num;i++) {
+		factorial*=i;
+		
+	}
+System.out.println(factorial);
+
+}
+}
