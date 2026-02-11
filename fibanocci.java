ad7ebdd57eb2b66acd682550ef2c1209284398da
Pasham Varshitha <varshithapasham552@gmail.com>
2026-02-11 21:20:01 +0530

    Add files via upload

---
+++ fibanocci.java
@@ -0,0 +1,15 @@
+package helloworld;
+
+public class fibanocci {
+	public static void main(String[] args) {
+		int num1=2;
+		int num2=3;
+		int sum = num1+num2;
+		num1=num2;
+		num2=sum;
+		System.out.println(sum);
+		
+	}
+
+}
+ 
