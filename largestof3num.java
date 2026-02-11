c91a2a1a2aa20bc1283339c5b15a234bd003e592
Pasham Varshitha <varshithapasham552@gmail.com>
2026-02-11 21:18:25 +0530

    Add files via upload

---
+++ largestof3num.java
@@ -0,0 +1,21 @@
+package helloworld;
+
+public class largestof3num {
+	public static void main(String[] args) {
+		int num1=2;
+		int num2=5;
+		int num3=1;
+		if (num1>=num2 && num1>=num3 ) {
+		System.out.print(num1);
+		}
+		else if(num2>=num1 && num2>=num3) {
+			System.out.print(num2);
+			
+		}
+		else {	
+			System.out.print(num3);
+			
+		}
+	}
+
+}
